b4a8458fea337e45503b3a8202d26be9169ae527
G186452021 <218645@st.takushoku-u.ac.jp>
2023-07-06 14:38:10 +0900

    数当て

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -8,15 +8,14 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click',hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let i = document.querySelector('input[name="kaito"]');
+  let yoso = i.value;
   kaisu = kaisu +1
 
   let s1 = document.querySelector('span#kaisu');
